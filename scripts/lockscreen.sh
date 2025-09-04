2da8d068f58dfaab8c0b3307691b2e2a320105d3
bakir.ali <bakir.ali@baly.iq>
2025-09-04 08:46:13 +0300

    fix/resize image in lockscreen.sh

--- scripts/lockscreen.sh
+++ scripts/lockscreen.sh
@@ -4,8 +4,7 @@
 tmpbg="/tmp/lockscreen.png"
 
 # Apply blur effect
-magick /home/$USER/Pictures/wallpapers/bg_20.jpg -resize 1920x1080^ -gravity center -extent 1920x1080 \
-  -scale 10% -blur 0x4 -scale 1000% "$tmpbg"
+magick /home/$USER/Pictures/wallpapers/bg_20.jpg -scale 10% -blur 0x4 -scale 1000% "$tmpbg"
 
 # Lock the screen with blurred background
 i3lock -i "$tmpbg"
